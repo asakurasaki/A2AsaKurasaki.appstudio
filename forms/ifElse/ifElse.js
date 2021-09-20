66d295265ba8366fc7bbeb1adc54f8e3a4c5b39e
asakurasaki <90635229+asakurasaki@users.noreply.github.com>
2021-09-19 19:45:47 -0500

    Finished code

---
+++ forms/ifElse/ifElse.js
@@ -0,0 +1,21 @@
+let firstName = prompt("What's your first name?")
+console.log(firstName)
+
+let state = prompt("What's ur state in 2 letters?")
+console.log(state)
+
+let temp = prompt("What's the wheather outside in F?")
+console.log(temp)
+
+let messages = [`${firstName}, wear a warm coat, hat, scarf and gloves.`,`${firstName}, wear a warm coat but you won't need a hat, scarf or gloves.`,`${firstName}, wear your warmest coat, a warm hat, a scarf, and warm gloves.`,`${firstName}, wear a warm coat, hat and gloves. Maybe a scarf too.`]
+console.log(messages)
+
+if (temp < 32 && state == 'NE')
+  console.log(messages[0])
+else if (temp >= 32 && temp <= 50 && state == 'NE')
+  console.log(messages[1])
+else if (temp >= 32 && temp <= 50 && state == 'FL')
+else if (temp > 50 && temp <= 70 && state == 'FL')
+  console.log(messages[3])
+else
+  console.log("Don't currently have a recommendation.")
